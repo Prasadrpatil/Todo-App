1fba1cd00fe5dc025ec2458924bb37a1690684fe
Prasad Patil <prasadrpatil25.10@gmail.com>
2020-09-19 19:20:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
+import Todo from "./Todo";
+import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  // when app loads , we need to listen DB aand fetch the new todos
+  useEffect(() => {
+    // this code here fires when app loads
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo }))
+        );
+      });
+  }, []);
+
+  const addTodo = (event) => {
+    //this will fire off when we click the button
+
+    event.preventDefault();
+    db.collection("todos").add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setTodos([...todos, input]);
+    setInput(""); //clears the input after hitting button
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Todo App 🚀!</h1>
+      <form>
+        <FormControl>
+          <InputLabel>✔️ Write A Todo</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+        </FormControl>
+
+        <Button
+          disabled={!input}
+          type="submit"
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
         >
-          Learn React
-        </a>
-      </header>
+          Add todo
+        </Button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <Todo todo={todo}></Todo>
+        ))}
+      </ul>
     </div>
   );
 }
